dc921482d9c17801c8175f6801dc0253536d75b0
Hubert Woszczyk <woszczykh@gmail.com>
2019-10-23 17:03:23 +0200

    Heads or tails callback

---
+++ actions.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from snipsTools import SnipsConfigParser
+from hermes_python.hermes import Hermes
+from hermes_python.ontology.dialogue.intent import IntentMessage
+
+import random
+
+CONFIG_INI = "config.ini"
+MQTT_IP_ADDR = "localhost"
+MQTT_PORT = 1883
+MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
+
+class DiceThrower(object):
+    def __init__(self):
+        try:
+            self.config = SnipsConfigParser.read_configuration_file(CONFIG_INI)
+        except Exception:
+            self.config = None
+
+        self.start_blocking()
+
+    def HeadsOrTailsCallback(self, hermes: Hermes, intent_message: IntentMessage):
+        hermes.publish_end_session(intent_message.session_id, "")
+
+        heads = random.sample(range(0, 1), 1)
+        answer = "Face" if heads == 1 else "Pile"
+
+        hermes.publish_start_session_notification(intent_message.site_id, answer, "")
+
+    def ThrowDiceCallback(self, hermes: Hermes, intent_message: IntentMessage):
+        pass
+
+    # register callback function to its intent and start listen to MQTT bus
+    def start_blocking(self):
+        with Hermes(MQTT_ADDR) as h:
+            h.subscribe_intent("hjwk:HeadsOrTails", self.HeadsOrTailsCallback)
+            h.subscribe_intent("hjwk:ThrowDice", self.ThrowDiceCallback)
+            h.loop_forever()
+
+if __name__ == "__main__":
+    DiceThrower()
