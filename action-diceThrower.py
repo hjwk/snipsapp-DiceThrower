58fde2421de7e4e333d98a736d68015c58a6827a
Hubert Woszczyk <woszczykh@gmail.com>
2019-10-23 19:51:48 +0200

    Added GiveLastThrowParams and RepeatThrow callback

--- action-diceThrower.py
+++ action-diceThrower.py
@@ -20,6 +20,20 @@ class DiceThrower(object):
             self.config = None
 
         self.start_blocking()
+        self.last_type = 6
+        self.last_number = 1
+
+    def GenerateAnswer(self, numberOfDices, typeOfDice):
+        answer = ""
+        for i in range(numberOfDices):
+            result = random.randint(0, typeOfDice)
+            answer += "{}".format(result)
+            if i == (numberOfDices - 2):
+                answer += " et "
+            elif i < (numberOfDices - 2):
+                answer += ", "
+        
+        return answer
 
     def HeadsOrTailsCallback(self, hermes: Hermes, intent_message: IntentMessage):
         hermes.publish_end_session(intent_message.session_id, "")
@@ -34,19 +48,24 @@ class DiceThrower(object):
 
         numberOfDicesSlot = extractSlot(intent_message.slots, "numberOfDices")
         numberOfDices = numberOfDicesSlot if numberOfDicesSlot != None else 1
+        self.last_number = numberOfDices
 
         diceTypeSlot = extractSlot(intent_message.slots, "diceType")
         diceType = diceTypeSlot if diceTypeSlot != None else 6
+        self.last_type = diceType
 
-        answer = ""
-        for i in range(numberOfDices):
-            result = random.randint(0, diceType)
-            answer += "{}".format(result)
-            if i == (numberOfDices - 2):
-                answer += " et "
-            elif i < (numberOfDices - 2):
-                answer += ", "
+        answer = self.GenerateAnswer(numberOfDices, diceType)
+        hermes.publish_start_session_notification(intent_message.site_id, answer, "")
 
+    def GiveLastThrowParamsCallback(self, hermes: Hermes, intent_message: IntentMessage):
+        hermes.publish_end_session(intent_message.session_id, "")
+
+        hermes.publish_start_session_notification(intent_message.site_id, "J'ai lancé {} dés {}".format(self.last_number, self.last_type), "")
+
+    def RepeatThrowCallback(self, hermes: Hermes, intent_message: IntentMessage):
+        hermes.publish_end_session(intent_message.session_id, "")
+
+        answer = self.GenerateAnswer(self.last_number, self.last_type)
         hermes.publish_start_session_notification(intent_message.site_id, answer, "")
 
     # register callback function to its intent and start listen to MQTT bus
@@ -54,6 +73,8 @@ class DiceThrower(object):
         with Hermes(MQTT_ADDR) as h:
             h.subscribe_intent("hjwk:HeadsOrTails", self.HeadsOrTailsCallback)
             h.subscribe_intent("hjwk:ThrowDice", self.ThrowDiceCallback)
+            h.subscribe_intent("hjwk:GiveLastThrowParams", self.GiveLastThrowParamsCallback)
+            h.subscribe_intent("hjwk:RepeatThrow", self.RepeatThrowCallback)
             h.loop_forever()
 
 if __name__ == "__main__":
